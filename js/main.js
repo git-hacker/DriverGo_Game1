0dfd0d0935adb486a320d0cb2f7e4c4e05feda5a
Steven Jackson <steve@sjackson.net>
2018-09-07 22:05:55 +0800

    Set up base project
    A lot of this boilerplate code was taken from the default WeChat
    MiniGame project that comes w/ the dev tools.

---
+++ js/main.js
@@ -0,0 +1,71 @@
+import DataBus from './databus'
+
+let ctx = canvas.getContext('2d')
+let databus = new DataBus()
+
+export default class Main {
+  constructor() {
+    this.restart()
+
+    this.aniId = window.requestAnimationFrame(
+      this.bindLoop,
+      canvas
+    )
+  }
+
+  restart() {
+    databus.reset()
+
+    this.bindLoop = this.loop.bind(this)
+  }
+
+  collisionDetection() {
+    console.log("Col det");
+  }
+
+  touchEventHandler(e) {
+    e.preventDefault()
+
+    let x = e.touches[0].clientX
+    let y = e.touches[0].clientY
+
+    let area = this.gameinfo.btnArea
+
+    if (x >= area.startX
+      && x <= area.endX
+      && y >= area.startY
+      && y <= area.endY)
+      this.restart()
+  }
+
+  render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = "#ffffff"
+    ctx.font = "20px Arial"
+
+    ctx.fillText(
+      "Cool beans buddy",
+      50,
+      50
+    )
+  }
+
+  // 游戏逻辑更新主函数
+  update() {
+    this.collisionDetection()
+  }
+
+  // 实现游戏帧循环
+  loop() {
+    databus.frame++
+
+    this.update()
+    this.render()
+
+    this.aniId = window.requestAnimationFrame(
+      this.bindLoop,
+      canvas
+    )
+  }
+}
