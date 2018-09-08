429c5dda4976d804027f5b3cf1a818ec635fd0ec
Steven Jackson <steve@sjackson.net>
2018-09-08 17:54:45 +0800

    Clean up code and replace background image

--- js/background/index.js
+++ js/background/index.js
@@ -4,35 +4,25 @@ const screenWidth = window.innerWidth
 const screenHeight = window.innerHeight
 
 const BG_IMG_SRC = 'images/bg.png'
-const BG_WIDTH = 512
-const BG_HEIGHT = 512
+const BG_WIDTH = 114
+const BG_HEIGHT = 122
 
-/**
- * 游戏背景类
- * 提供update和render函数实现无限滚动的背景功能
- */
 export default class BackGround extends Sprite {
   constructor(ctx) {
-    super(BG_IMG_SRC, BG_WIDTH, BG_HEIGHT)
+    super(BG_IMG_SRC, BG_WIDTH, BG_HEIGHT);
 
-    this.top = 0
+    this.top = 0;
 
-    this.render(ctx)
+    this.render(ctx);
   }
 
   update() {
-    this.top += 2
+    this.top += 2;
 
     if (this.top >= screenHeight)
-      this.top = 0
+      this.top = 0;
   }
 
-  /**
-   * 背景图重绘函数
-   * 绘制两张图片，两张图片大小和屏幕一致
-   * 第一张漏出高度为top部分，其余的隐藏在屏幕上面
-   * 第二张补全除了top高度之外的部分，其余的隐藏在屏幕下面
-   */
   render(ctx) {
     ctx.drawImage(
       this.img,
@@ -44,7 +34,7 @@ export default class BackGround extends Sprite {
       -screenHeight + this.top,
       screenWidth,
       screenHeight
-    )
+    );
 
     ctx.drawImage(
       this.img,
@@ -56,6 +46,6 @@ export default class BackGround extends Sprite {
       this.top,
       screenWidth,
       screenHeight
-    )
+    );
   }
 }
--- js/player/index.js
+++ js/player/index.js
@@ -4,23 +4,16 @@ import DataBus from '../databus'
 const screenWidth = window.innerWidth
 const screenHeight = window.innerHeight
 
-// 玩家相关常量设置
 const PLAYER_IMG_SRC = 'images/truck.jpeg'
 const PLAYER_WIDTH = 32
 const PLAYER_HEIGHT = 109
 
-let databus = new DataBus()
 
 export default class Player extends Sprite {
   constructor() {
     super(PLAYER_IMG_SRC, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-    // 玩家默认处于屏幕底部居中位置
-    this.x = screenWidth / 2 - this.width / 2
     this.y = screenHeight - this.height - 30
-
-    // 用于在手指移动的时候标识手指是否已经在飞机上了
-    this.touched = false
 
     this.currentRoad = 2;
   }
